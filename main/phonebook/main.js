38705b5fa3049ebcb9188b73283dff377b38a39d
ColinJung <master@entercube.net>
2022-07-08 00:58:54 +0900

    v0.62

---
+++ main/phonebook/main.js
@@ -0,0 +1,8 @@
+// Path : phonebook    
+// Table : phonebook
+// Api name : phonebook_api (Public Opened API CORS with * ) 
+// Stage : $default 
+
+const apiInvokeBaseURL = "https://tdfont1pd2.execute-api.ap-northeast-2.amazonaws.com"; 
+const apiBaseDirName =  "main/phonebook"; 
+ 
---
+++ main/phonebook5_jwt/main.js
@@ -0,0 +1,10 @@
+// Path : phonebook5   
+// Table : phonebook
+// Api name : phonebook5_api 
+// CORS : * 
+// Authorizer : JWT
+// Stage : v1 ( not auto deploy ) 
+
+const apiInvokeBaseURL = "https://2fqs7xljhd.execute-api.ap-northeast-2.amazonaws.com/v1"
+const apiBaseDirName =  "main/phonebook5_jwt"; 
+ 
