7d603270ef4ed16a8453369bbe56d03669b2a8ac
ColinJung <master@entercube.net>
2022-07-13 15:14:54 +0900

    v0.72

--- main/phonebook/main.js
+++ main/phonebook/main.js
@@ -3,6 +3,12 @@
 // Api name : phonebook_api (Public Opened API CORS with * ) 
 // Stage : $default 
 
+
+// HTTP-API Phonebook DB호출 
 const apiInvokeBaseURL = "https://tdfont1pd2.execute-api.ap-northeast-2.amazonaws.com"; 
+
+// REST-API Phonebook2 DB호출 
+// const apiInvokeBaseURL = "https://8p4o30hace.execute-api.ap-northeast-2.amazonaws.com/v1"
+
 const apiBaseDirName =  "main/phonebook"; 
  
