0c12793331fba0f01d8c1f2cf4c77ac0f748bb9b
ColinJung <master@entercube.net>
2022-07-22 10:13:02 +0900

    v1.07

--- main/phonebook5_jwt/main.js
+++ main/phonebook5_jwt/main.js
@@ -5,6 +5,6 @@
 // Authorizer : JWT
 // Stage : v1 ( not auto deploy ) 
 
-const apiInvokeBaseURL = "https://2fqs7xljhd.execute-api.ap-northeast-2.amazonaws.com/v1"
+const apiInvokeBaseURL = "https://ktx1rquiu1.execute-api.ap-northeast-2.amazonaws.com"
 const apiBaseDirName =  "main/phonebook5_jwt"; 
  
